8992bd385ccb4c325580a15ce22abbfc332ec3b6
Ayyappan Mani <ayyappan.mani@outlook.in>
2022-06-26 12:19:50 +0530

    ...

--- configure_devops_system.py
+++ configure_devops_system.py
@@ -12,6 +12,7 @@ def main():
     sequence_of_configuration_dict = ({'note':'\n[+] update the package lists\n', 'config_command':'sudo apt update', 'failed_note':'\n[-] unable to update the package\n'},
                                       {'note':'\n[+] install package installer for python\n', 'config_command':'sudo apt install -y python3-pip', 'failed_note':'\n[-] python3-pip installation is failed\n'},
                                       {'note':'\n[+] install netmiko library\n', 'config_command':'pip install netmiko', 'failed_note':'\n[-] unable to install netmiko library\n'},
+                                      {'note':'\n[+] install napalm library\n', 'config_command':'pip install napalm', 'failed_note':'\n[-] unable to install napalm library\n'},
                                       {'note':'\n[+] install apache2\n', 'config_command':'sudo apt install -y apache2', 'failed_note':'\n[-] apache2 installation is failed\n'},
                                       {'note':'\n[+] disable apache2\'s mpm_event module\n', 'config_command':'sudo a2dismod mpm_event', 'failed_note':'\n[-] unable to disable the module\n'},
                                       {'note':'\n[+] enable apache2\'s mpm_prefork and cgi module\n', 'config_command':'sudo a2enmod mpm_prefork cgi', 'failed_note':'\n[-] unable to enable the module\n'},
